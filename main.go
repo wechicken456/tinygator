e05f2639775ad5a0e5487ec35e949e8e88aa470f
Tin Vuong <vuonghieunghia19122003@gmail.com>
2024-12-10 20:20:42 -0500

    added sqlc to manage database migration

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"gator/internal/config"
+	"gator/internal/database"
 	"log"
 	"os"
+	"time"
+
+	"github.com/google/uuid"
+	_ "github.com/lib/pq" // underscore tells Go that you're importing it for its side effects, not because you need to use it.
 )
 
 type state struct {
 	conf *config.Config
+	db   *database.Queries
 }
 
 type command struct {
@@ -32,9 +40,36 @@ func (c *commands) run(s *state, cmd command) error {
 	return errors.New(fmt.Sprintf("[!] Command '%v' doesn't exist!", cmd.name))
 }
 
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("Empty arguments!! Need a username.")
+	}
+
+	// check if user already existsed
+	_user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if _user != (database.User{}) {
+		return errors.New(fmt.Sprintf("[!] User %v already exists!", cmd.args[0]))
+	}
+
+	// insert new user into database
+	user := database.CreateUserParams{}
+	user.ID = uuid.New()
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	user.Name = cmd.args[0]
+
+	_, err = s.db.CreateUser(context.Background(), user)
+	return err
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return errors.New("Empty arguments!! Need a username.")
+	}
+	// check if user already existsed
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return errors.New(fmt.Sprintf("[!] User %v doesn't exists!", cmd.args[0]))
 	}
 	return s.conf.SetUser(cmd.args[0])
 }
@@ -45,9 +80,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	STATE := &state{conf: conf}
+	db, err := sql.Open("postgres", conf.DB_url)
+	dbQueries := database.New(db)
+
+	STATE := &state{conf: conf, db: dbQueries}
 	COMMANDS := &commands{handler: make(map[string]func(*state, command) error)}
 	COMMANDS.register("login", handlerLogin)
+	COMMANDS.register("register", handlerRegister)
 	_args := os.Args
 	if len(_args) < 2 {
 		log.Fatal("Forgot an argument?...")
@@ -57,6 +96,7 @@ func main() {
 	if err := COMMANDS.run(STATE, COMMAND); err != nil {
 		log.Fatal(err)
 	}
+	conf.SetUser(_args[2])
 	conf, err = config.Read()
 	if err != nil {
 		log.Fatal(err)
